127f55655506d156201c5136fd99f2bb94499bca
Jean DUPIN <jean.dupin69@gmail.com>
2023-12-01 12:50:48 +0100

    Day 1 solution

---
+++ 2023/Day1.R
@@ -0,0 +1,55 @@
+# Input ----
+input <-
+  read.table("2023/Inputs/Day1.txt")
+
+
+# Part 1 ----
+solution_1 <-
+  gsub("\\D","",
+     input$V1) |> 
+  (\(.){
+    paste0(
+      substr(.,1,1),
+      substr(.,nchar(.),nchar(.))
+    )
+  })() |> 
+  as.numeric() |> 
+  sum(); solution_1
+
+
+# Part 2 ----
+mots = c(paste0(1:9),
+         "one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
+
+
+premier <-
+  sapply(mots,
+       FUN = function(.x){
+         gregexpr(.x,input$V1) |> 
+           sapply(FUN = function(.y){ifelse(min(.y) == -1,
+                                            NA,
+                                            min(.y))})
+       }) |> 
+  apply(MARGIN = 1,
+        FUN = function(.x){
+          c(1:9,1:9)[which.min(.x)]
+        })
+
+dernier <-
+  sapply(mots,
+         FUN = function(.x){
+           gregexpr(.x,input$V1) |> 
+             sapply(FUN = function(.y){ifelse(min(.y) == -1,
+                                              NA,
+                                              max(.y))})
+         }) |> 
+  apply(MARGIN = 1,
+        FUN = function(.x){
+          c(1:9,1:9)[which.max(.x)]
+        })
+
+solution_2 <-
+  paste0(premier, dernier) |> 
+  as.numeric() |> 
+  sum(); solution_2
+
