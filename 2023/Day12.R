aae41f0184c4adc5e5c1957ad6d3ba1625dd2e62
Jean DUPIN <jean.dupin69@gmail.com>
2023-12-13 02:14:31 +0100

    2023 - Day 12 / Part 2 w/ help

--- 2023/Day12.R
+++ 2023/Day12.R
@@ -58,5 +58,89 @@ solution1 <-
   sum(solutions)
 
 
+# Partie 2 ----
+# Source : https://github.com/Naturage/Advent-of-code-2023/blob/main/Day%2012/Scripts/Day%2012%20attempt%202.R
 
+patternes <-
+  sapply(input, function(.x){
+    gsub(" .*$","",.x) |> 
+      rep(5) |> 
+      paste(collapse = "?") |>
+      (\(.){strsplit(.,"")[[1]]})()
+  }, USE.NAMES = F, simplify = F)
+
+
+positions <-
+  sapply(input, function(.x){
+    gsub("^.* ","",.x) |>
+      (\(.){strsplit(.,",")[[1]]})() |> 
+      as.numeric() |> 
+      rep(5)
+  }, USE.NAMES = F, simplify = F)
+
+
+
+
+bigsolver <- function(.x, .y){
+  
+  positions <<-
+    matrix(rep(-1, (length(.x)+1) * (length(.y) + 1)),
+           nrow = (length(.x) + 1))
+  
+  for (i in 1:length(.x)){
+    positions[i, length(.y) + 1] <<-
+      ifelse(all(.x[i:length(.x)] %in% c(".","?")), 1, 0)
+  }
+  
+  positions[length(.x) + 1, ] <<-
+    0
+  positions[length(.x) + 1, length(.y) + 1] <<-
+    1
+  
+  for (i in seq(length(.x), 1, by = -1)){
+    for (j in seq(length(.y), 1, by = -1)){
+      MAJ.position(i, j, .x, .y)
+    }
+  }
+  return(positions[1,1])
+}
+
+
+
+
+MAJ.position <- function(i, j, .x, .y){
+  
+  positions[i, j] <<-
+    0
+  next_string <-
+    .y[j]
+  
+  if(.x[i] %in% c(".","?")){
+    positions[i, j] <<-
+      positions[i, j] + positions[i + 1, j]
+  }
+  if (.x[i] %in% c("#","?")){
+    # but is too short, do nothing.
+    if (i + next_string > length(.x) + 1){
+      
+    } else if (i + next_string == length(.x) + 1){
+      if (all(.x[i:(i - 1 + next_string)] %in% c("?","#"))){
+        positions[i, j] <<-
+          positions[i, j] + positions[length(.x) + 1, j + 1]
+      }
+    } else {
+      if ((all(.x[i:(i - 1 + next_string)] %in% c("?","#"))) & (.x[i + next_string] %in% c(".","?"))){
+        positions[i, j] <<-
+          positions[i, j] + positions[i + next_string + 1, j + 1]
+      }
+    }
+  }
+  return(NULL)
+}
+
+solution2 <-
+  mapply(bigsolver, 
+         patternes,
+         positions) |> 
+  sum()
 
