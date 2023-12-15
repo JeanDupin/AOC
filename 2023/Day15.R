ddf915632be8256e7f972ca13c33e1941436a05f
Jean DUPIN <jean.dupin69@gmail.com>
2023-12-15 09:26:15 +0100

    2023 - Day 15

---
+++ 2023/Day15.R
@@ -0,0 +1,86 @@
+# Input ----
+
+input <-
+  readLines("2023/Inputs/Day15.txt") |> 
+  (\(.){strsplit(.,",")[[1]]})()
+  
+
+# Partie 1 ----
+
+hash <-
+  function(caractere){
+    valeur <<- 
+      ((valeur + utf8ToInt(caractere))*17) %% 256
+  }
+
+valeurs <- c()
+for(i in seq_along(input)){
+  consignes <-
+    strsplit(input[i],"")[[1]]
+  valeur <- 0
+  sapply(consignes,
+         hash, USE.NAMES = F)
+  valeurs <- append(valeurs,
+                    valeur)
+}; rm(i, consignes,
+      valeur)
+
+solution1 <-
+  sum(valeurs); rm(valeurs)
+
+
+# Partie 2 ----
+
+boxes <-
+  vector("list",256) |> 
+  (\(.){`names<-`(.,c(0:255))})()
+
+
+change_boite <-
+  function(.x){
+    label.lens <-
+      gsub("[^a-z]*","",.x)
+    
+    for(i in 1:nchar(label.lens)){
+      consignes <-
+        strsplit(label.lens,"")[[1]]
+      valeur <<- 0
+      sapply(consignes,
+             hash, USE.NAMES = F)
+    }
+    
+    if(grepl("=",.x)){
+      valeur.lens <-
+        as.numeric(gsub("[^1-9]*","",.x))
+      if(label.lens %in% names(boxes[[as.character(valeur)]])){
+        boxes[[as.character(valeur)]][label.lens] <<- valeur.lens
+      } else {
+        boxes[[as.character(valeur)]] <<-
+          append(boxes[[as.character(valeur)]],
+                 c(valeur.lens) |> (\(.){`names<-`(.,label.lens)})())
+      }
+    }
+    
+    if(grepl("-",.x)){
+      if(label.lens %in% names(boxes[[as.character(valeur)]])){
+        boxes[[as.character(valeur)]] <<-
+          boxes[[as.character(valeur)]][-which(names(boxes[[as.character(valeur)]]) == label.lens)]
+      } else {}
+    }
+  }
+
+invisible(sapply(input, change_boite)); rm(valeur)
+
+valeurs <- c()
+for(i in seq_along(boxes)){
+  if(length(boxes[[i]]) == 0){
+    valeurs[i] = 0
+  } else {
+    valeurs[i] <-
+      sum(i * boxes[[i]] * c(1:length(boxes[[i]])))
+  }
+}; rm(i)
+
+solution2 <-
+  sum(valeurs); rm(valeurs)
+
