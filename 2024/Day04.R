5ab56aab38370b701e4408dcfdaa7efe0715c05b
Jean Dupin <jean.dupin69@gmail.com>
2024-12-04 08:53:53 +0100

    2024 - Jour 4

--- 2024/Day04.R
+++ 2024/Day04.R
@@ -3,11 +3,10 @@
 input <-
   httr2::request("https://adventofcode.com/2024/day/4/input") |> 
   httr2::req_cookies_set(session = Sys.getenv("aoc_cookie")) |> 
+  httr2::req_cache(tempdir(),debug = T) |> 
   httr2::req_perform() |> 
   httr2::resp_body_string() |> 
   (\(.){strsplit(.,"\\n")[[1]]})()
-
-input = clipr::read_clip()
 
 # Partie 1 ----
 
@@ -16,133 +15,130 @@ matrice <-
   unlist() |> 
   matrix(ncol = nchar(input[1]),byrow = T)
 
-
-a <-
-  apply(matrice,1,function(.x){
-  a <-
-  paste(.x,collapse = "") |>
-    (\(.){regmatches(.,
-                     gregexpr("XMAS",.))[[1]]})() |> 
-    length()
-  b <-
-  paste(rev(.x),collapse = "") |>
-    (\(.){regmatches(.,
-                     gregexpr("XMAS",.))[[1]]})() |> 
-    length()
-  
-  a + b
-  }) |> 
-  sum()
-
-b <-
-  apply(matrice,2,function(.x){
-  a <-
-    paste(.x,collapse = "") |>
-    (\(.){regmatches(.,
-                     gregexpr("XMAS",.))[[1]]})() |> 
-    length()
-  b <-
-    paste(rev(.x),collapse = "") |>
-    (\(.){regmatches(.,
-                     gregexpr("XMAS",.))[[1]]})() |> 
-    length()
-  
-  a + b
-}) |> 
-  sum()
-
-d <-
-  lapply(
-  split(matrice,(row(matrice) - col(matrice))),
-  function(.x){
-    a <-
-      paste(.x,collapse = "") |>
-      (\(.){regmatches(.,
-                       gregexpr("XMAS",.))[[1]]})() |> 
-      length()
-    b <-
-      paste(rev(.x),collapse = "") |>
-      (\(.){regmatches(.,
-                       gregexpr("XMAS",.))[[1]]})() |> 
-      length()
+get_xmas <-
+  function(matrice){
     
-    a + b
-  }
-) |> 
-  unlist() |> 
-  sum()
-
-e <-
-  lapply(
-  split(matrice,(row(matrice) + col(matrice))),
-  function(.x){
-    a <-
-      paste(.x,collapse = "") |>
-      (\(.){regmatches(.,
-                       gregexpr("XMAS",.))[[1]]})() |> 
-      length()
-    b <-
-      paste(rev(.x),collapse = "") |>
-      (\(.){regmatches(.,
-                       gregexpr("XMAS",.))[[1]]})() |> 
-      length()
+    nrows <-
+      apply(matrice,1,function(.x){
+        a <-
+          paste(.x,collapse = "") |>
+          (\(.){regmatches(.,
+                           gregexpr("XMAS",.))[[1]]})() |> 
+          length()
+        b <-
+          paste(rev(.x),collapse = "") |>
+          (\(.){regmatches(.,
+                           gregexpr("XMAS",.))[[1]]})() |> 
+          length()
+        
+        a + b
+      }) |> 
+      sum()
     
-    a + b
+    ncols <-
+      apply(matrice,2,function(.x){
+        a <-
+          paste(.x,collapse = "") |>
+          (\(.){regmatches(.,
+                           gregexpr("XMAS",.))[[1]]})() |> 
+          length()
+        b <-
+          paste(rev(.x),collapse = "") |>
+          (\(.){regmatches(.,
+                           gregexpr("XMAS",.))[[1]]})() |> 
+          length()
+        
+        a + b
+      }) |> 
+      sum()
+    
+    ndiags1 <-
+      lapply(
+        split(matrice,(row(matrice) - col(matrice))),
+        function(.x){
+          a <-
+            paste(.x,collapse = "") |>
+            (\(.){regmatches(.,
+                             gregexpr("XMAS",.))[[1]]})() |> 
+            length()
+          b <-
+            paste(rev(.x),collapse = "") |>
+            (\(.){regmatches(.,
+                             gregexpr("XMAS",.))[[1]]})() |> 
+            length()
+          
+          a + b
+        }
+      ) |> 
+      unlist() |> 
+      sum()
+    
+    ndiags2 <-
+      lapply(
+        split(matrice,(row(matrice) + col(matrice))),
+        function(.x){
+          a <-
+            paste(.x,collapse = "") |>
+            (\(.){regmatches(.,
+                             gregexpr("XMAS",.))[[1]]})() |> 
+            length()
+          b <-
+            paste(rev(.x),collapse = "") |>
+            (\(.){regmatches(.,
+                             gregexpr("XMAS",.))[[1]]})() |> 
+            length()
+          
+          a + b
+        }
+      ) |> 
+      unlist() |> 
+      sum()
+    
+    nrows + ncols + ndiags1 + ndiags2
   }
-) |> 
-  unlist() |> 
-  sum()
-
-
 
 
 solution1 <-
-  a + b + d + e
+ get_xmas(matrice)
 
 # Partie 2 ----
 
-
-d <-
-  lapply(
-    split(matrice,(row(matrice) - col(matrice))),
-    function(.x){
-      a <-
-        paste(.x,collapse = "") |>
-        (\(.){regmatches(.,
-                         gregexpr("MAS",.))[[1]]})() |> 
-        length()
-      b <-
-        paste(rev(.x),collapse = "") |>
-        (\(.){regmatches(.,
-                         gregexpr("MAS",.))[[1]]})() |> 
-        length()
-      
-      list(a,b)
+get_mas <-
+  function(matrice){
+    solution = 0
+    for(i in 1:(ncol(matrice)-2)){
+      for(j in 1:(nrow(matrice)-2)){
+        if(matrice[i,j] == "S"){
+          if(matrice[i+2,j+2] == "M" & matrice[i+1,j+1] == "A"){
+            if(matrice[i+2,j] == "M" & matrice[i,j+2] == "S"){
+              solution = solution + 1
+            } else if (matrice[i+2,j] == "S" & matrice[i,j+2] == "M"){
+              solution = solution + 1
+            } else {
+              next
+            }
+          } else {
+            next
+          }
+        } else if(matrice[i,j] == "M"){
+          if(matrice[i+2,j+2] == "S" & matrice[i+1,j+1] == "A"){
+            if(matrice[i+2,j] == "S" & matrice[i,j+2] == "M"){
+              solution = solution + 1
+            } else if (matrice[i+2,j] == "M" & matrice[i,j+2] == "S"){
+              solution = solution + 1
+            } else {
+              next
+            }
+          } else {
+            next
+          }
+        } else {
+          next
+        }
+      }
     }
-  ) |> 
-  unlist() |> 
-  sum()
+    solution
+  }
 
-e <-
-  lapply(
-    split(matrice,(row(matrice) + col(matrice))),
-    function(.x){
-      a <-
-        paste(.x,collapse = "") |>
-        (\(.){regmatches(.,
-                         gregexpr("MAS",.))[[1]]})() |> 
-        length()
-      b <-
-        paste(rev(.x),collapse = "") |>
-        (\(.){regmatches(.,
-                         gregexpr("MAS",.))[[1]]})() |> 
-        length()
-      
-      a + b
-    }
-  ) |> 
-  unlist() |> 
-  sum()
-  
 solution2 <-
-  NA
+  get_mas(matrice)
