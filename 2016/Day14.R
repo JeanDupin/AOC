af54488e89fd0cac181dc6d0e83ee2dc8d981016
Jean DUPIN <jean.dupin69@gmail.com>
2025-09-04 23:06:13 +0200

    2016 - Jour 14

--- 2016/Day14.R
+++ 2016/Day14.R
@@ -16,16 +16,16 @@ patterne <-
 
 patterne2 <-
   c(
-    paste(letters,letters,letters,letters,
+    paste(letters,letters,letters,letters,letters,
           sep = ""),
-    paste(0:9,0:9,0:9,0:9,
+    paste(0:9,0:9,0:9,0:9,0:9,
           sep = "")
   )
 
 
-i = 1
+i = 0
 keys = vector("numeric")
-while(i <= 23000){
+while(length(keys) < 64){
   if(i %% 1000 == 0){print(i)}
   hash <-
     digest::digest(paste0(input,i),
@@ -51,7 +51,7 @@ while(i <= 23000){
                             serialize = F) |> 
                (\(.){
                  regmatches(.,
-                            gregexpr("(.)\\1\\1\\1?",.))[[1]][1]
+                            gregexpr("(.)\\1\\1\\1?\\1?",.))[[1]][1]
                })()
            }) |> 
     (\(.){any(.[!is.na(.)] == next_patterne)})()
@@ -64,13 +64,60 @@ while(i <= 23000){
 
 
 solution1 <-
-  NA
-
-send_solution(2016,14,1,solution1)
+  keys[64]
 
 # Partie 2 ----
 
-solution2 <-
-  NA
+super_hash <- function(x){
+  for(i in seq_len(2017)){
+    x <- openssl::md5(x) 
+  }
+  x
+}
+
+cache_hashes <- vector("character",30000)
+for(i in 1:30000){
+  cache_hashes[i] <- as.character(super_hash(paste0(input,i)))
+  if(i %% 1000 == 0){print(i)}
+}
+
+
+i = 1
+keys = vector("numeric")
+while(length(keys) < 64){
+  if(i %% 100 == 0){print(i)}
+  hash <-
+    cache_hashes[i]
   
-send_solution(2016,14,2,solution2)
+  if(!grepl(paste(patterne, collapse = "|"),hash)){
+    i = i + 1
+    next
+  }
+  
+  # Quel patterne ?
+  next_patterne <-
+    regmatches(hash,
+               gregexpr("(.)\\1\\1",hash))[[1]][1] |> 
+    (\(.){patterne2[which(patterne == .)]})() 
+  
+  if(length(next_patterne) > 1){break}
+  
+  is_key <-
+    cache_hashes[(i+1):(i+1000)] |> 
+    (\(.){
+      regmatches(.,
+                 gregexpr("(.)\\1\\1\\1?\\1?",.))
+    })() |> 
+    (\(.){any(unlist(.) == next_patterne)})()
+  
+  
+  if(is_key){
+    keys = append(keys, i)
+    print(i)
+  }
+  i = i + 1
+}
+
+
+solution2 <-
+  keys[64]
