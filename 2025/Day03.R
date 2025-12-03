7b9184085f089d5729ba79ab8b74965a74121770
Jean Dupin <jean.dupin69@gmail.com>
2025-12-03 08:45:49 +0100

    2025 - Jour 3 - Better solution

--- 2025/Day03.R
+++ 2025/Day03.R
@@ -6,48 +6,29 @@ input <-
 
 # Partie 1 ----
 
-solution1 <-
-  sapply(
-    input,
-    function(.x){
-      res = 0
-      nombres = as.numeric(strsplit(.x,"")[[1]])
-      for(i in seq_along(nombres)[-length(nombres)]){
-        for(j in seq_along(nombres)[-seq_len(i)]){
-          if(i == j){next}
-          res <-
-            max(res,
-                10*nombres[i] + nombres[j])
-        }
-      }
-      res
-    },
-    USE.NAMES = F,
-    simplify = T
-  ) |> 
-  sum()
-
-# Partie 2 ----
-
-max_sequence <- function(.x){
+max_sequence <- function(.x, n){
   nombres <- as.integer(strsplit(.x, "")[[1]])
-  n <- length(nombres)
-  taille <- n - 12
+  taille <- length(nombres) - n
   res <- vector("integer")
   
   for(i in nombres){
     while(length(res) > 0 &&
-           res[length(res)] < i &&
-           taille > 0){
+          res[length(res)] < i &&
+          taille > 0){
       res <- res[-length(res)]
       taille <- taille - 1
     }
     res <- c(res, i)
   }
-  as.numeric(paste0(res[seq_len(12)], collapse = ""))
+  as.numeric(paste0(res[seq_len(n)], collapse = ""))
 }
 
+solution1 <-
+  sapply(input, max_sequence, n = 2, USE.NAMES = F, simplify = T) |> 
+  sum()
+
+# Partie 2 ----
 
 solution2 <-
-  sapply(input, max_sequence, USE.NAMES = F, simplify = T) |> 
+  sapply(input, max_sequence, n = 12, USE.NAMES = F, simplify = T) |> 
   sum()
